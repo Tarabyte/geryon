47e07f955e527d65963d8afdf15d1116ea8336eb
tarabyte <tarabanko.yury@gmail.com>
2014-05-05 18:49:44 +0100

    Registers and memory

--- geryon.js
+++ geryon.js
@@ -1,3 +1,45 @@
-(function(){
-  Γ= {};  
-}());
+(function(global){
+    "use strict";
+    var word = 10,
+        base = 3,
+        len = Math.pow(base, word);
+    
+    function G() {
+        var a, c, d, memory;
+        if(!(this instanceof G)) {
+            return new G();
+        }
+        
+        a = c = d = 0;
+        
+        memory = new Array(len);
+        
+        Object.defineProperties(this, {
+            a: {
+                get: function() {
+                    return a;
+                }
+            },
+            
+            c: {
+                get: function() {
+                    return c;
+                }
+            },
+            
+            d: {
+                get: function() {
+                    return d;
+                }
+            },
+            
+            memory: {
+                get: function() {
+                    return memory.slice(0);    
+                }
+            }
+        });        
+    }
+    
+    global.Γ = G;
+}(this));
--- test/geryon_test.js
+++ test/geryon_test.js
@@ -1,5 +1,49 @@
 module("Geryon");
 
 test('Should be defined', function() {
-   ok(Γ, 'is defined'); 
+    equal(typeof Γ, 'function', 'is defined');
+    
+    var g = Γ();
+    
+    ok(g, 'got new machine');
+    ok(g instanceof Γ, 'can be called without new');
+});
+
+test('Registers', function() {
+    var g = Γ(), registers = "acd".split("");
+    
+    registers.forEach(function(item) {
+        ok(item in g, item + ' register is defined');
+        equal(g[item], 0, 'initial value is 0');    
+        
+        try {
+            g[item] = "somevalue";
+        }
+        catch (e) {        
+            equal(g[item], 0, 'readonly');    
+        }
+        
+    });    
+});
+
+test('Memory', function() {
+    var g = Γ(),
+        memory = g.memory,
+        length = Math.pow(3, 10);
+    
+    ok(Array.isArray(memory), 'memory is array');
+    
+    equal(memory.length, length, 'Memory length is 3^10');
+    
+    memory.length = 0;
+    
+    memory = g.memory;
+    equal(memory.length, length, 'Memory is immutable');
+    
+    try {
+        g.memory = [];
+    } catch (e) {
+        ok(true, 'Memory is readonly');
+    }
+    
 });
