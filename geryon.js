0f539d0a827c57dd8950b6b8647152a659df14ff
tarabyte <tarabanko.yury@gmail.com>
2014-05-08 14:37:58 +0100

    Program loading, normalization, denormalization.

--- geryon.js
+++ geryon.js
@@ -3,9 +3,11 @@
     var word = 10,
         base = 3,
         len = Math.pow(base, word),
+        eof = len - 1,
         proto = G.prototype,
         start = 33,
-        encrypted = "5z]&gqtyfr$(we4{WP)H-Zn,[%\\3dL+Q;>U!pJS72FhOA1CB6v^=I_0/8|jsb9m<.TVac`uY*MK'X~xDl}REokN:#?G\"i@".split(''),
+        empty = '',
+        encrypted = "5z]&gqtyfr$(we4{WP)H-Zn,[%\\3dL+Q;>U!pJS72FhOA1CB6v^=I_0/8|jsb9m<.TVac`uY*MK'X~xDl}REokN:#?G\"i@".split(empty),
         makeArray = function(){
             return Array.apply(null, new Array(word));
         },
@@ -14,18 +16,61 @@
                 return (num - (num %= val))/val;
             });
         },
-        crz = [[1, 0, 0], [1, 0, 2], [2, 2, 1]],
+        crzTable = [[1, 0, 0], [1, 0, 2], [2, 2, 1]],
         pows = makeArray().map(function(_, i){
             return Math.pow(base, word - 1 - i);
-        });
+        }),
+        instructions = {
+            4: 'i',
+            5: '<',
+            23: '/',
+            39: '*',
+            40: 'j',
+            62: 'p',
+            68: 'o',
+            81: 'v'
+        },
+        alphabetLen = 94,
+        isCommand = function(op, idx) {
+            return getCode(op, idx) in instructions;    
+        }, 
+        toNumber = function(val) {
+            return typeof val == 'number' ? val : (val+empty).charCodeAt(0);
+        },
+        getCode = function(op, idx) {
+            op = toNumber(op);
+                
+            return (op + (idx||0)) % alphabetLen;
+        },
+        normalized = (function(){
+            return Object.keys(instructions).reduce(function(memo, key){
+                memo[instructions[key]] = key;
+                return memo;
+            }, {});
+        }()),
+        isSpace = (function(){
+            var spaces = [9, 10, 13, 32];
+            return function(code) {
+                return spaces.indexOf(code) > -1;
+            };
+        }()),
+        crz = function(a, b) {
+            a = toTritArray(a);
+            b = toTritArray(b);
+        
+            return pows.reduce(function(c, base, i) {
+                return c + base * crzTable[a[i]][b[i]];
+            }, 0);
+        };
     
-    function G() {
-        var a, c, d, memory;
+    function G(options) {
+        var a, c, d, memory, input;
         if(!(this instanceof G)) {
-            return new G();
+            return new G(options);
         }
         
         a = c = d = 0;
+        options = options || {};
         
         memory = new Array(len);
         
@@ -53,28 +98,79 @@
                     return memory.slice(0);    
                 }
             }
-        });        
+        });  
+        
+        /**
+         * Copy value to A register.
+         */
+        this.input = function(value) {
+            value = toNumber(value);
+            
+            a = value == -1 ? eof : value;
+        };
+        
+        /**
+         * Load a program code to memory.
+         */ 
+        this.load = function(code) {
+            var errors = [], i, prev, preprev, commands = [], skipped = 0;
+            (code + empty).split(empty).forEach(function(code, position) {
+                var command = code.charCodeAt(0);
+                
+                if(isSpace(command)) { //skip spaces.
+                    skipped++;
+                    return;    
+                }
+                if(!isCommand(command, position - skipped)) { //check if a command is actually a command
+                    errors.push({
+                        position: position,
+                        code: code
+                    });
+                }
+                
+                commands.push(command);
+            });
+            
+            
+            if(errors.length) {
+                throw new Error('Error on parsing program:' + code + '\nCaused by: ' + errors.map(function(error){return error.code + ' at ' + error.position;})); 
+            }
+            if(commands.length > len) {
+                throw new Error('Input program is too long. Length: ' + commands.length);    
+            }
+            
+            commands.forEach(function(command, position) { //copy program to memory
+                memory[position] = command;    
+            });
+            
+            //implementation specific behaviour for 1 char programs. 
+            //http://stackoverflow.com/questions/23524597
+            if(commands.length == 1) {
+                memory[1] = 0;
+            }
+            
+            i = Math.max(2, commands.length);
+            prev = memory[i - 1];
+            preprev = memory[i - 2];
+            
+            for(; i < len; ++i) { //fill memory using crz.
+                memory[i] = prev = crz(preprev, preprev = prev);
+            }
+        };
     }
     
     /**
      * Encrypt a symbol specified.
      */
     proto.encrypt = function(symbol) {
-        return encrypted[('' + symbol).charCodeAt(0) - start];    
+        return encrypted[(empty + symbol).charCodeAt(0) - start];    
     };
     
     /**
      * Perform crazy tritwise operation.
      * 
      */
-    proto.crz = function(a, b) {
-        a = toTritArray(a);
-        b = toTritArray(b);
-        
-        return pows.reduce(function(c, base, i) {
-            return c + base * crz[a[i]][b[i]];
-        }, 0);
-    };
+    proto.crz = crz;
     
     /**
      * Tritwise rotate right.
@@ -86,6 +182,65 @@
         return pows.reduce(function(c, base, i){
             return c + base * a[i];
         }, 0);
+    };
+    
+    /**
+     * Get value at A register.
+     */
+    proto.atA = function() {
+        var a = this.a;
+        return a === eof ? -1 : String.fromCharCode(a % 256);
+    };
+    
+    /**
+     * Denormalizing code.
+     */
+    proto.denormalize = function(code) {
+        var skipped = 0;
+        return (code + empty).split(empty).map(function(code, position){
+            var command = code.charCodeAt(0);
+            if(isSpace(command)) {
+                skipped++;
+                return code;
+            }
+            
+            command = normalized[code];
+            
+            if(command) {
+                position -= skipped %= alphabetLen;
+                command -= position;
+                while(command < start) {
+                    command += alphabetLen;
+                }
+                
+                return String.fromCharCode(command);
+            }
+            
+            throw new Error('Symbol ' + code + ' at ' + position + ' is illegal.');
+                
+        }).join(empty);
+    };
+    
+    /**
+     * Normalizing code.
+     */
+    proto.normalize = function(code) {
+        var skipped = 0;
+        return (code + empty).split(empty).map(function(code, position) {
+            var command = code.charCodeAt(0);
+            if(isSpace(command)) {
+                skipped++;
+                return code;    
+            }
+            command = instructions[getCode(command, position - skipped)];
+            
+            if(command) {
+                return command;   
+            }
+            
+            throw new Error('Symbol ' + code + ' at ' + position + ' is illegal.');
+            
+        }).join(empty);
     };
     
     
--- test/geryon_test.js
+++ test/geryon_test.js
@@ -82,3 +82,179 @@ test('rotr', function() {
     
     equal(g.rotr(trit('0001112221')), trit('1000111222'), '0001112221 --> 1000111222');
 });
+
+test('input', function() {
+    var value = "c",
+        g = new Γ();
+    
+    equal(typeof g.input, 'function', 'input function is defined');
+    
+    g.input(value);
+    
+    equal(g.a, value.charCodeAt(0), 'value moved to A register');
+    
+    g.input('\n');
+    
+    equal(g.a, 10, 'newline copied to A register');
+    
+    g.input(-1);
+    equal(g.a, 59048, 'EOF copied to A register');  
+});
+
+test('atA', function() {
+    var g = Γ(),
+        value = "g";
+    
+    g.input(value);
+    
+    equal(typeof g.atA, 'function', 'atA is defined');
+    
+    equal(g.atA(), value, 'atA returns symbols');
+    
+    g.input(-1);
+    
+    equal(g.atA(), -1, 'atA returns symbols');
+    
+    g.input(10+256);
+    equal(g.atA(), '\n', 'atA performs modulo 256');
+    
+});
+
+var helloWorld1 = "(=<`:9876Z4321UT.-Q+*)M'&%$H\"!~}|Bzy?=|{z]KwZY44Eq0/{mlk**hKs_dG5[m_BA{?-Y;;Vb'rR5431M}/.zHGwEDCBA@98\\6543W10/.R,+O<",
+    helloWorld2 = "('&%:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9wv6WsU2T|nm-,jcL(I&%$#\"`CB]V?Tx<uVtT`Rpo3NlF.Jh++FdbCBA@?]!~|4XzyTT43Qsqq(Lnmkj\"Fhg${z@>",
+    
+commands = [4 + 94, 5 + 94, 23 + 94, 39, 40, 62, 68, 81].map(function(code){
+    return String.fromCharCode(code);
+}),
+    mnemonics = "ji*p</vo".split(''); 
+
+test('load', function() {
+    var g = Γ();
+    
+    equal(typeof g.load, 'function', 'load function is defined');
+    
+    throws(function(){
+        g.load("=");
+    }, 'can not contain non-command symbols');
+    
+    commands.forEach(function(code) {        
+        var command = code.charCodeAt(0);
+        g.load(code);
+        ok(true, code + ' loaded ok');
+        
+        code += String.fromCharCode(command - 1);
+        g.load(code);
+        ok(true, code + ' loaded ok');
+    });
+    
+    
+    
+    g.load(helloWorld1);
+    
+    var memory = g.memory;
+    
+    helloWorld1.split('').forEach(function(code, position) {
+        equal(String.fromCharCode(memory[position]), code, code + ' copied to position ' + position);    
+    });
+    
+    g.load('(');
+    memory = g.memory;
+    
+    //@see http://stackoverflow.com/questions/23524597
+    equal(memory[1], 0, 'Implementations specific');
+    
+    equal(memory.filter(function(current, idx) {
+        return idx < 2 || current == g.crz(memory[idx-2], memory[idx-1]);
+    }).length, memory.length, 'memory is f*cked up');
+});
+
+test('load should skip spaces', function() {
+    var g = Γ(); 
+    
+    g.load(' (\t\n=\r\n<');
+    ok(true, 'should not throw errors');
+    
+});
+
+function makeArray(len) {
+    return Array.apply(null, new Array(len));
+}
+
+
+function randomCommand() {
+    return commands[Math.random()*commands.length|0];
+}
+
+function randomMnemonic() {
+    return mnemonics[Math.random()*mnemonics.length|0];
+}
+test('normalize|denormalize', function() {
+    var g = Γ();
+    
+    equal(typeof g.denormalize, 'function', 'denormalize is a function');
+    
+    var code = makeArray(1000).map(function() {
+        return randomMnemonic();
+    }).join('');
+    
+    
+    var denormalized = g.denormalize(code);
+    
+    g.load(denormalized);
+    ok(true, 'denormalized code loaded');
+    
+    //mixin random spaces 
+    var spaces = " \t\r\n".split('');
+    
+    code = code.split('').map(function(item, idx) {
+        if(idx%25) {
+            return item;            
+        }
+        return spaces[Math.random()*spaces.length|0];
+    }).join('');
+    
+    denormalized = g.denormalize(code);
+    
+    equal(denormalized.length, code.length, 'spaces were not stripped');
+    
+    g.load(denormalized);
+    ok(true, 'denormalize works with spaces');
+    
+    equal(typeof g.normalize, 'function', 'normalize is a function');
+    
+    var normalized = g.normalize(denormalized);
+    
+    equal(normalized, code, 'normalized code equals to original');
+    
+    var cat = "(aBA@?>=<;:9876543210/.-,JH)('&%$#\"!~}|{zy\\J6utsrq\nponmlkjihgJ%dcba`_^]\\[ZYXWVUTSRQPONMLKJIHGF('C%$$^\nK~<;4987654321a/.-,\\*)\nj\n!~%|{zya}|{zyxwvutsrqSonmlO\njLhg`edcba`_^]\\[ZYXWV8TSRQ4\nONM/KJIBGFE>CBA@?>=<;{9876w\n43210/.-m+*)('&%$#\"!~}|{zy\\\nwvunslqponmlkjihgfedcEa`_^A\n\\>ZYXWPUTSRQPONMLKJIH*FEDC&\nA@?>=<;:9876543210/.-m+*)(i\n&%$#\"!~}|{zyxwvutsrqpRnmlkN\nihgfedcba`_^]\\[ZYXWVU7SRQP3\nNMLKJIHGFEDCBA@?>=<;:z8765v\n3210/.-,+*)('&%$#\"!~}_{zyx[\nvutsrqjonmlejihgfedcba`_^]@\n[ZYXWVUTSRo",
+        normCat = "jiooooooooooooooooooooooo<ioooooooooooooooj/iooooo\noooooooooojiooooooooooooooooooooooooooooooo**o**j<\nv*oopooooooooo/oooo/oo\nj\nppopppp*ooooooooooooo*ooooj\no*oopoooooooooooooooo*ooooj\nooo*ooopooopooooooooo*ooooj\noooooooo*oooooooooooooooooj\nooopopooooooooooooooo*ooooj\no*oooopoooooooooooooo*ooooj\nooooooooooooooooooooo*ooooj\nooooooooooooooooooooo*ooooj\nooooooooooooooooooooo*ooooj\nooooooooooooooooooooo*ooooj\nooooooooooooooooooooo*ooooj\noooooopoooopooooooooooooooj\nooooooooooi";
+    
+    g.load(cat);
+    
+    equal(g.normalize(cat), normCat, 'cat normalized');
+    
+    equal(g.denormalize(normCat), cat, 'cat back and force');
+    
+});
+
+test('load should throw on large inputs', function() {
+    var g = Γ(),
+        longProgram = g.denormalize(makeArray(Math.pow(3,10)).map(randomMnemonic).join(''));
+    
+    //console.log(longProgram);
+    
+    g.load(longProgram);
+    ok(true, 'loaded');
+    
+    longProgram = "   " + longProgram + " \t";
+    g.load(longProgram);
+    ok(true, 'loaded');
+    
+    longProgram = g.denormalize(makeArray(Math.pow(3,10)+1).map(randomMnemonic).join(''));
+    
+    throws(function() {
+        g.load(longProgram);    
+    }, /too long/, 'throw on programs larger than mem capacity');
+    
+    
+});
